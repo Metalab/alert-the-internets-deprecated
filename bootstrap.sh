dcfe6fe0b818e651d96049f91a06365d5f47ba1e
sixtyeight <christian_sl@gmx.at>
2016-12-10 06:23:50 +0100

    added debian backports repo and ffmpeg package dependency

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# ffmpeg available in backports
+echo "bootstrap: creating /etc/apt/sources.list.d/alert-the-internets.list"
+echo "deb http://ftp.de.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/alert-the-internets.list
+
 # install ansible which is used for all other provisioning of the vm
 echo "bootstrap: installing ansible"
 apt-get install -y ansible > /dev/null
